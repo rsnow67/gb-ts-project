19660e78293f99ac61a1c3abf84fc4827972eb60
Vadim Vidman <vidman07@mail.ru>
2022-05-29 20:26:57 +0300

    update function renderSearchFormBlock

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,18 @@ import { renderToast } from './lib.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   renderUserBlock('Wade Warren', '/img/avatar.png', 0);
-  renderSearchFormBlock();
+
+  const checkInDefaultDate = new Date();
+  checkInDefaultDate.setDate(checkInDefaultDate.getDate() + 1);
+  const checkInDefaultDateISO = new Date(checkInDefaultDate.getTime() - (checkInDefaultDate.getTimezoneOffset() * 60000))
+    .toISOString().split('T')[0];
+
+  const checkOutDefaultDate = new Date();
+  checkOutDefaultDate.setDate(checkInDefaultDate.getDate() + 2);
+  const checkOutDefaultDateISO = new Date(checkOutDefaultDate.getTime() - (checkOutDefaultDate.getTimezoneOffset() * 60000))
+    .toISOString().split('T')[0];
+
+  renderSearchFormBlock(checkInDefaultDateISO, checkOutDefaultDateISO);
   renderSearchStubBlock();
   renderToast(
     { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
