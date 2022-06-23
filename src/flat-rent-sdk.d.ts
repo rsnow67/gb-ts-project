11e4551c1ebe7f896f4ca983da9a0bd6b3cda466
Vadim Vidman <vidman@starline.ru>
2022-06-23 18:25:56 +0300

    add types for private methods of class FlatRentSdk

--- src/flat-rent-sdk.d.ts
+++ src/flat-rent-sdk.d.ts
@@ -25,8 +25,16 @@ export interface SearchParameters {
 
 export class FlatRentSdk {
   get(id: string): Promise<Flat | null>;
-
   search(parameters: SearchParameters): Flat[];
-
   book(flatId: number, checkInDate: Date, checkOutDate: Date): number;
+  private _assertDatesAreCorrect(checkInDate: Date, checkOutDate: Date): void;
+  private _resetTime(date: Date): void;
+  private _calculateDifferenceInDays(startDate: Date, endDate: Date): number;
+  private _generateDateRange(from: Date, to: Date): Date[];
+  private _generateTransactionId(): number;
+  private _areAllDatesAvailable(flat: Flat, dateRange: Date[]): boolean;
+  private _formatFlatObject(flat: Flat, nightNumber?: number): Flat;
+  private _readDatabase(): Flat[];
+  private _writeDatabase(database: Flat[]): void;
+  private _syncDatabase(database: Flat[]): void;
 }
